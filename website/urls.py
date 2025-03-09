04fd4273ba2811f1dd73baaad5cded5426acfbfa
DaniMarques1 <daniel.13.marques.18@gmail.com>
2025-03-09 01:29:26 -0300

    Adição de páginas e sistema de Login

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import home
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
-    path("", home, name="home"),
-] 
+    path("login/", login_view, name="login"),
+    path("logout/", LogoutView.as_view(next_page="/login/"), name="logout"),
+    path("", redirect_to_menu, name="home"),
+    
+    path("menu/", menu, name="menu"),
+    path("cadastro/cestas/", cadastro_cestas, name="cadastro_cestas"),
+    path("cadastro/familias/", cadastro_familias, name="cadastro_familias"),
+    path("cadastro/produtos/", cadastro_produtos, name="cadastro_produtos"),
+]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-def home(request):
-    return render(request, "home.html")
+def redirect_to_menu(request):
+    """Redirect root URL to /menu."""
+    return redirect("menu")
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('menu')
+    
+    if 'next' in request.GET:
+        messages.warning(request, "Você deve fazer login para acessar a página desejada.")
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET.get('next') or 'menu')
+        else:
+            messages.error(request, "Credenciais inválidas. Tente novamente.")
+    
+    return render(request, 'login.html')
+
+# @login_required(login_url='/login/')
+def menu(request):
+    return render(request, "menu.html")
+
+# @login_required(login_url='/login/')
+def cadastro_cestas(request):
+    return render(request, "cadastro_cestas.html")
+
+# @login_required(login_url='/login/')
+def cadastro_familias(request):
+    return render(request, "cadastro_familias.html")
+
+# @login_required(login_url='/login/')
+def cadastro_produtos(request):
+    return render(request, "cadastro_produtos.html")
